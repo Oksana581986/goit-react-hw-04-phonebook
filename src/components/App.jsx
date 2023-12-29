9d66eaec609361e5061ae4646357db72cd029fc1
Oksana581986 <oksanaotchych@gmail.com>
2023-12-29 20:01:35 +0200

    reacthw04phbk

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import { useState, useEffect } from 'react';
 import { nanoid } from 'nanoid'
 import { ContactForm } from 'components/contactForm/ContactForm';
 import { Filter } from 'components/filter/Filter';
